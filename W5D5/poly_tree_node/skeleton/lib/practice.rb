a95499f8a1f0b4dc1fdc5b6634a41be4c8ff6660
Bradley <capitalb@live.com>
2019-04-19 16:38:45 -0500

    brad barnes_aima malik poly tree  node complete

---
+++ W5D5/poly_tree_node/skeleton/lib/practice.rb
@@ -0,0 +1,27 @@
+def merge_sort(array)
+    return array if array.length < 2
+
+    mid = array.length / 2
+    left
+end
+
+
+
+def bfs(array, target)
+
+    array.each do |ele|
+        array.each do |ele|
+
+
+end
+
+def dfs(root, target)
+    return if root == target
+    root.chilren.each do |child|
+        dfs(child, target)
+    end
+end
+
+
+
+array to track to initial pos?
