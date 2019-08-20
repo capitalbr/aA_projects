193376040947fc3ce5e4b9cc7827b57b66f668f5
Bradley <bbarnes.me@gmail.com>
2019-08-20 17:04:53 -0500

    Complete Stack project

--- W23D2/stack_project/lib/stack.js
+++ W23D2/stack_project/lib/stack.js
@@ -21,11 +21,48 @@
 // -----------
 
 class Node {
-
+  constructor(val, next = null){
+    this.value = val;
+    this.next = next;
+  }
 }
 
 class Stack {
+  constructor(top = null, bottom = null, length = 0) {
+    this.top = top; 
+    this.bottom = bottom;
+    this.length = length;
+  }
 
+  push(val){
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.bottom = newNode;
+      newNode.next = null;
+    } else { 
+      newNode.next = this.top;
+    }
+    this.top = newNode;
+    this.length += 1;
+    return this.length;
+  }
+  
+  pop(){
+    if (this.length === 0) return null;
+    let topEl = this.top;
+    this.top = topEl.next;
+    
+    this.length -= 1;
+    if (this.length === 0) {
+      this.top = null;
+      this.bottom = null;
+    }
+    return topEl.value;
+  }
+
+  size(){
+    return this.length;
+  }
 }
 
 exports.Node = Node;
