07205920cffa9eaaf1c564662cba1aa1d72bf4a6
Bradley <bbarnes.me@gmail.com>
2019-08-20 17:15:29 -0500

    Complete Queue project

--- W23D2/queue_project/lib/queue.js
+++ W23D2/queue_project/lib/queue.js
@@ -21,11 +21,49 @@
 // -----------
 
 class Node {
-
+  constructor(val, next = null){
+    this.value = val;
+    this.next = next;
+  }
 }
 
 class Queue {
+  constructor(back = null, front = null, length = 0){
+    this.back = back;
+    this.front = front;
+    this.length = length;
+  }
 
+  enqueue(val){
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.front = newNode;
+    } else {
+      this.back.next = newNode;
+    }
+    newNode.next = null;
+    this.back = newNode;
+    this.length += 1;
+    return this.length;
+  }
+
+// removing from adt determines how node.next will be implemented
+  dequeue(){
+    if (this.length === 0) return null;
+    
+    let oldFront = this.front;
+    this.front = oldFront.next;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.front = null;
+      this.back = null;
+    }
+    return oldFront.value;
+  }
+
+  size(){
+    return this.length;
+  }
 }
 
 exports.Node = Node;
